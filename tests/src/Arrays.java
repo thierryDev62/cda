3acfb8c01caee453f4d6ff8bd907562e1827aa16
thierrydev62 <thierrydev62@gmail.com>
2020-09-01 18:16:22 +0200

    update

--- tests/src/Arrays.java
+++ tests/src/Arrays.java
@@ -1,8 +1,8 @@
 public class Arrays {
     public static void main(String[] arg) {
-        String[] tableauMots = {"chien", "chat"};
-        for(int i = 0; i < tableauMots.length; i++){
-            System.out.println(tableauMots[i]);
+        String[] tableauMots = {"chien", "chat", "poisson"};
+        for (String tableauMot : tableauMots) {
+            System.out.println(tableauMot);
         }
     }
 }
