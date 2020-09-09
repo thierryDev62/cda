469b5f69132b4e6e1c9c81cbb6925185536b74f7
thierrydev62 <thierrydev62@gmail.com>
2020-09-09 18:23:35 +0200

    update

--- tests/src/Traitement.java
+++ tests/src/Traitement.java
@@ -4,13 +4,11 @@ import fr.cda.data.Personne;
 
 public class Traitement {
     public static void main(String[] arg) {
+        Personne personne1 = new Personne("Thonon", "James", 26, 0);
         Permis permisPersonne1 = new Permis();
-        permisPersonne1.setNbrAnneesPermis(2);
-
         Fidelite fidelitePersonne1 = new Fidelite();
-        fidelitePersonne1.setAnciennete(15);
-
-        Personne personne1 = new Personne("Thonon", "James", 40, 0);
+        permisPersonne1.setNbrAnneesPermis(3);
+        fidelitePersonne1.setAnciennete(6);
 
         personne1.setPermis(permisPersonne1);
         personne1.setFidelite(fidelitePersonne1);
@@ -22,16 +20,17 @@ public class Traitement {
                 "\nAncienneté : " + personne1.getFidelite().getAnciennete() + " ans" +
                 "\nNombre d'accident : " + personne1.getNbrAccident()
                 );
-       calcul(personne1.getAge(), personne1.getPermis().getNbrAnneesPermis(), personne1.getNbrAccident(), personne1.getFidelite().getAnciennete());
+       calcul(personne1.getAge(), permisPersonne1.getNbrAnneesPermis(), personne1.getNbrAccident(), fidelitePersonne1.getAnciennete());
 
     }
     public static void calcul(int age, int anneesPermis, int nombreAccident, int anciennete){
         boolean ageSup25, totalAccident0, totalAccident1, totalAccident2, clientFidele, plusDeuxAnsPermis;
         int tarifRouge = 200, tarifOrange = 150, tarifVert = 100, tarifBleu = 50;
+        String ANSI_RED = "\u001B[31m", ANSI_GREEN = "\u001B[32m", ANSI_BLUE = "\u001B[34m", ANSI_ORANGE = "\u001B[33m", ANSI_RESET = "\u001B[0m";
 
         /* Booleens des conditions */
         ageSup25 = age >= 25;
-        plusDeuxAnsPermis = anneesPermis >= 2;
+        plusDeuxAnsPermis = anneesPermis > 2;
         totalAccident0 = nombreAccident == 0;
         totalAccident1 = nombreAccident == 1;
         totalAccident2 = nombreAccident == 2;
@@ -41,42 +40,40 @@ public class Traitement {
         if(!ageSup25 && !plusDeuxAnsPermis) {
             if(totalAccident0) {
                 if(!clientFidele) {
-                    System.out.println("Le tarif rouge est appliqué soit " + tarifRouge + "€");
+                    System.out.println("Le tarif " + ANSI_RED + "rouge" + ANSI_RESET + " est appliqué soit " + tarifRouge + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifRouge + "€\nMontant après remise : " + remise(tarifRouge, anciennete) + "€");
                     }
                 } else {
-                    System.out.println("Le tarif orange est appliqué");
-                    if(anciennete > 0) {
-                        System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
-                    }
+                    System.out.println("Le tarif " + ANSI_ORANGE + "orange" + ANSI_RESET + " est appliqué");
+                    System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
                 }
             } else {
                 System.out.println("Désolé mais c'est un refus");
             }
 
             /* Cas 2 */
-        } else if(!ageSup25 && plusDeuxAnsPermis || ageSup25 && !plusDeuxAnsPermis) {
+        } else if(!ageSup25 || !plusDeuxAnsPermis) {
             if(totalAccident0) {
                 if(!clientFidele) {
-                    System.out.println("Le tarif orange est appliqué soit " + tarifOrange + "€");
+                    System.out.println("Le tarif " + ANSI_ORANGE + "orange" + ANSI_RESET + " est appliqué soit " + tarifOrange + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
                     }
                 } else {
-                    System.out.println("Le tarif vert est appliqué soit " + tarifVert + "€");
+                    System.out.println("Le tarif " + ANSI_GREEN + "vert" + ANSI_RESET + " est appliqué soit " + tarifVert + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifVert + "€\nMontant après remise : " + remise(tarifVert, anciennete) + "€");
                     }
                 }
             } else if(totalAccident1) {
                 if(!clientFidele) {
-                    System.out.println("Le tarif rouge est appliqué soit " + tarifRouge + "€");
+                    System.out.println("Le tarif " + ANSI_RED + "rouge" + ANSI_RESET + " est appliqué soit " + tarifRouge + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifRouge + "€\nMontant après remise : " + remise(tarifRouge, anciennete) + "€");
                     }
                 } else {
-                    System.out.println("Le tarif orange est appliqué soit " + tarifOrange + "€");
+                    System.out.println("Le tarif " + ANSI_ORANGE + "orange" + ANSI_RESET + " est appliqué soit " + tarifOrange + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
                     }
@@ -86,42 +83,36 @@ public class Traitement {
             }
 
             /* Cas 3 */
-        } else if(ageSup25 && plusDeuxAnsPermis) {
+        } else {
             if(totalAccident0) {
                 if(!clientFidele) {
-                    System.out.println("Le tarif vert est appliqué soit " + tarifVert + "€");
+                    System.out.println("Le tarif " + ANSI_GREEN + "vert" + ANSI_RESET + " est appliqué soit " + tarifVert + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifVert + "€\nMontant après remise : " + remise(tarifVert, anciennete) + "€");
                     }
                 } else {
-                    System.out.println("Le tarif bleu est appliqué soit " + tarifBleu + "€");
-                    if(anciennete > 0) {
-                        System.out.println("Montant avant remise : " + tarifBleu + "€\nMontant après remise : " + remise(tarifBleu, anciennete) + "€");
-                    }
+                    System.out.println("Le tarif " + ANSI_BLUE + "bleu" + ANSI_RESET + " est appliqué soit " + tarifBleu + "€");
+                    System.out.println("Montant avant remise : " + tarifBleu + "€\nMontant après remise : " + remise(tarifBleu, anciennete) + "€");
                 }
             } else if(totalAccident1) {
                 if(!clientFidele) {
-                    System.out.println("Le tarif orange est appliqué soit " + tarifOrange + "€");
+                    System.out.println("Le tarif " + ANSI_ORANGE + "orange" + ANSI_RESET + " est appliqué soit " + tarifOrange + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
                     }
                 } else {
-                    System.out.println("Le tarif vert est appliqué soit " + tarifVert + "€");
-                    if(anciennete > 0) {
-                        System.out.println("Montant avant remise : " + tarifVert + "€\nMontant après remise : " + remise(tarifVert, anciennete) + "€");
-                    }
+                    System.out.println("Le tarif " + ANSI_GREEN + "vert" + ANSI_RESET + " est appliqué soit " + tarifVert + "€");
+                    System.out.println("Montant avant remise : " + tarifVert + "€\nMontant après remise : " + remise(tarifVert, anciennete) + "€");
                 }
             } else if(totalAccident2) {
                 if(!clientFidele) {
-                    System.out.println("Le tarif rouge est appliqué soit " + tarifRouge + "€");
+                    System.out.println("Le tarif " + ANSI_RED + "rouge" + ANSI_RESET + " est appliqué soit " + tarifRouge + "€");
                     if(anciennete > 0) {
                         System.out.println("Montant avant remise : " + tarifRouge + "€\nMontant après remise : " + remise(tarifRouge, anciennete) + "€");
                     }
                 } else {
-                    System.out.println("Le tarif orange est appliqué soit " + tarifOrange + "€");
-                    if(anciennete > 0) {
-                        System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
-                    }
+                    System.out.println("Le tarif " + ANSI_ORANGE + "orange" + ANSI_RESET + " est appliqué soit " + tarifOrange + "€");
+                    System.out.println("Montant avant remise : " + tarifOrange + "€\nMontant après remise : " + remise(tarifOrange, anciennete) + "€");
                 }
             } else {
                 System.out.println("Désolé mais c'est un refus");
