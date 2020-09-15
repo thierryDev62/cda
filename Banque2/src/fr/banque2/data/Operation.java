9faa7006f0ccaca5bb694990a26fc96e40a0a382
thierrydev62 <thierrydev62@gmail.com>
2020-09-15 16:21:05 +0200

    Update Operation.java

--- Banque2/src/fr/banque2/data/Operation.java
+++ Banque2/src/fr/banque2/data/Operation.java
@@ -40,8 +40,6 @@ public class Operation {
      */
     public static void versementSurUnCompte() {
 
-        //TConsole.toprintln(dateDuJour());
-
         TConsole.toprintln("*********************************************************" +
                 "\nVersement sur un compte" +
                 "\n*********************************************************"
