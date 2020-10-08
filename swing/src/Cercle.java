1c53d3f8d9b797357256e0ac7daa09a6f083eb5d
thierrydev62 <thierrydev62@gmail.com>
2020-10-08 08:44:29 +0200

    update

---
+++ swing/src/Cercle.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Cercle extends JPanel {
+    public void paintComponent(Graphics g) {
+        g.fillOval(20,20,50,50);
+    }
+}
--- swing/src/Fenetre.java
+++ swing/src/Fenetre.java
@@ -5,13 +5,17 @@ public class Fenetre extends JFrame {
     public Fenetre() {
         this.setTitle("Ma belle fenêtre"); // Titre de la fenêtre
         this.setSize(640, 480); // Taille de la fenêtre
-        this.setLocationRelativeTo(null); // La positionne au milieu de l'écran
-        this.setVisible(true); // La rend visible
+        this.setLocationRelativeTo(null); // La centre au milieu
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ferme la fenêtre quand on clique sur la croix
+        this.setVisible(true); // Affiche la fenêtre
 
-        // Instanciation d'un objet JPanel
+        /* Fond orange
         JPanel pan = new JPanel();
-        pan.setBackground(Color.ORANGE); // Un fond orange
-
+        pan.setBackground(Color.ORANGE);
         this.setContentPane(pan);
+        */
+         /* Affiche un cercle
+        this.setContentPane(new Cercle());
+          */
     }
 }
