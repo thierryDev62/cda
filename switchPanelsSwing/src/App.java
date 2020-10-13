d452e7acf64275d8ed2fb97978d8a019b2a6d1a6
thierrydev62 <thierrydev62@gmail.com>
2020-10-13 09:35:28 +0200

    update

--- switchPanelsSwing/src/App.java
+++ switchPanelsSwing/src/App.java
@@ -2,30 +2,90 @@ import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.plaf.synth.SynthLookAndFeel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class App extends JFrame{
+    CardLayout cl = new CardLayout();
+    JPanel content = new JPanel();
+
+    // Liste des noms de nos conteneurs pour la pile de cartes
+    String[] listContent = { "MENU_PANEL", "CARD_3", "CARD_2" };
 
     public App() {
-        super("Switch entre deux panels");
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.setSize(640, 480);
+        super("My first application");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(600,400);
         this.setLocationRelativeTo(null);
 
-        setLayout(new BorderLayout());
-        CardLayout cl = new CardLayout();
-        JPanel mainView = new JPanel(cl);
+        // On crée deux conteneurs de couleurs différentes
+        JPanel menuPanel = new JPanel();
+        menuPanel.setBackground(Color.ORANGE);
 
-        //mainView.add(new JPanel(), "empty");
-        mainView.add(new Panel1());
+        JButton bleu = new JButton("Vers le bleu");
+        bleu.addActionListener(this::switchPanelBlue);
+        JButton rouge = new JButton("Vers le rouge");
+        rouge.addActionListener(this::switchPanelRed);
 
-        this.add(mainView);
+        menuPanel.add(bleu);
+        menuPanel.add(rouge);
 
-        // Faire méthode avec bouton pour changer de panel
+        JPanel card1 = new JPanel();
+        card1.setBackground(Color.BLUE);
+
+        JLabel titreCard1 = new JLabel("Panel bleu", SwingConstants.CENTER);
+        JButton goToRed = new JButton("Panel rouge");
+        goToRed.addActionListener(this::switchPanelRed);
+
+        card1.setLayout(new GridLayout(2,1));
+        card1.add(titreCard1);
+        card1.add(goToRed);
+
+
+        JPanel card2 = new JPanel();
+        card2.setBackground(Color.RED);
+        JLabel titreCard2 = new JLabel("Panel bleu", SwingConstants.CENTER);
+        JButton goToBlue = new JButton("Panel bleu");
+        goToBlue.addActionListener(this::switchPanelBlue);
+
+        card2.setLayout(new GridLayout(2,1));
+        card2.add(titreCard2);
+        card2.add(goToBlue);
+
+        JPanel panelPrincipal = new JPanel();
+        JLabel titre = new JLabel("Switch entre deux panels");
+        JButton retourMenu = new JButton("Retour au menu principal");
+        retourMenu.addActionListener(this::retourMenuPrincipal);
+        panelPrincipal.add(titre);
+        panelPrincipal.add(retourMenu);
+
+        // On définit le layout
+        content.setLayout(cl);
+
+        // On ajoute les cartes à la pile avec un nom pour les retrouver
+        content.add(menuPanel, listContent[0]);
+        content.add(card1, listContent[1]);
+        content.add(card2, listContent[2]);
+
+        this.getContentPane().add(panelPrincipal, BorderLayout.NORTH);
+        this.getContentPane().add(content, BorderLayout.CENTER);
 
     }
 
+    private void retourMenuPrincipal(ActionEvent e) {
+        cl.show(content, listContent[0]);
+    }
+
+    private void switchPanelBlue(ActionEvent e) {
+        cl.show(content, listContent[1]);
+    }
+
+    private void switchPanelRed(ActionEvent e) {
+        cl.show(content, listContent[2]);
+    }
+
+
     public static void main(String[] args) throws UnsupportedLookAndFeelException {
-        // Applys a look'n feel
+        // Apply a look'n feel
         UIManager.setLookAndFeel(new NimbusLookAndFeel());
 
         // Start my window
