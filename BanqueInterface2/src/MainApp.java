9f8f7cb3d0ab7b7904c17c3074c5bf81280afabb
thierrydev62 <thierrydev62@gmail.com>
2020-11-02 20:15:12 +0100

    update

--- BanqueInterface2/src/MainApp.java
+++ BanqueInterface2/src/MainApp.java
@@ -1,12 +1,29 @@
+import config.ConfigDb;
 import templates.principal.App;
 
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class MainApp {
+    private static Connection connection;
+
+    // Méthode qui permet d'initialiser la connection
+    private static void initConnection() {
+        try {
+            connection = ConfigDb.getINSTANCE().connection();
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     // Main
     public static void main(String[] args) {
+        initConnection();
         // Applique un thème sympa
         try {
             UIManager.setLookAndFeel(new NimbusLookAndFeel());
